a7ae87be6ee743ce3bb3d8eddd5d523b64168e33
Jose Lopez <josemiguel.lopez@kbc.be>
2018-05-16 15:01:00 +0200

    Base project

--- app/src/main/java/com/jmlb0003/jokesappnd/MainActivity.java
+++ app/src/main/java/com/jmlb0003/jokesappnd/MainActivity.java
@@ -2,12 +2,19 @@ package com.jmlb0003.jokesappnd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void tellJoke(final View view) {
+        Toast.makeText(this, "Blablabla", Toast.LENGTH_SHORT).show();
+    }
+
 }

3302fa112e687864bd84d86b9e1ef0c88570c5de
Jose Lopez <jmlb0003@gmail.com>
2018-05-17 11:21:40 +0200

    Updating some stuff for the GCE endpoitns

--- backend/src/main/java/com/jmlb0003/backend/MyEndpoint.java
+++ backend/src/main/java/com/jmlb0003/backend/MyEndpoint.java
@@ -3,27 +3,29 @@ package com.jmlb0003.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.jmlb0003.jokeslib.Joker;
 
-import javax.inject.Named;
-
-/** An endpoint class we are exposing */
+/**
+ * An endpoint class we are exposing
+ */
 @Api(
-        name = "myApi",
+        name = "jokeApi",
         version = "v1",
         namespace = @ApiNamespace(
-                ownerDomain = "backend.builditbigger.gradle.udacity.com",
-                ownerName = "backend.builditbigger.gradle.udacity.com",
+                ownerDomain = "backend.jmlb0003.com",
+                ownerName = "backend.jmlb0003.com",
                 packagePath = ""
         )
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+    /**
+     * A simple endpoint method that returns a joke
+     */
+    @ApiMethod(name = "getJoke")
+    public MyBean getAJoke() {
+        final MyBean response = new MyBean();
+        response.setData(Joker.getJoke());
         return response;
     }
 
